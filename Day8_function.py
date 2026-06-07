33f30d4e05e36915e58924958527d24bd5c3fe08
Ramla beevi <muhsinaramlaz@gmail.com>
2026-06-07 17:47:54 +0530

    Create Day8_function.py
    Day 8: Learned functions + Japan USA roadmap

---
+++ Day8_function.py
@@ -0,0 +1,20 @@
+def greet(name):
+    print("konnichiwa,", name +"-san")
+    print("Genki desu ka?")
+    
+greet("Ramla")
+greet("Tokyo")
+
+def japan_intro(name,dream):
+    print("Watashi no namae wa ",name + "-san")
+    print("2031 nen ni",dream,"ni ikimasu")
+    
+    
+japan_intro("Ramla","Tokyo")
+japan_intro("Nammal","USA")
+
+def add (a,b):
+    return a+b
+    
+result = add(2031,1)
+print("Japan + 1 year =",result
