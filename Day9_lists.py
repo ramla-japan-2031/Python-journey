44f30b54a2bae840eacfcb137eaf6d75bcced41a
Ramla beevi <muhsinaramlaz@gmail.com>
2026-06-08 19:08:34 +0530

    Create Day9_lists.py

---
+++ Day9_lists.py
@@ -0,0 +1,34 @@
+empty_bag = []
+ramla_favourites = ["mango","burpees","coding","Japan"]
+print(ramla_favourites)
+workout_count = [100,40,75,20]
+
+japan_cities = ["Tokyo","Osaka"]
+japan_cities.append("Kyoto")
+print(japan_cities)
+japan_cities.remove("Osaka")
+print(japan_cities)
+print(len(japan_cities))
+
+for city in japan_cities:
+    print(f"Njan pokum {city}")
+    
+    
+    
+diploma_college = ["GPC kalamassery","CPTC","WPTC TVM"]
+diploma_college.append("MPTC vadakara")
+print("Njan apply cheyyunna colleges:",diploma_college)
+
+my_rank = 501042
+if my_rank < 5000 :
+    print("Backup college nokkam")
+    
+workout = [100,40,75,20]
+total_reps = workout[0] + workout[1] + workout[2] + workout[3]
+print("Innu total reps :", total_reps)
+
+
+my_app_no = 501042
+diploma_college.insert(0,"Dream college")
+print(diploma_college)
+print("Last college :",diploma_college[-1])
