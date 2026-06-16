471652da5a944bb55f87a947d23c9ac295419b2d
Ramla beevi <muhsinaramlaz@gmail.com>
2026-06-16 18:36:38 +0530

    Create Day2_variables.py
    Variables and Data types

---
+++ Day2_variables.py
@@ -0,0 +1,10 @@
+
+x = 10          # int
+y = 3.14        # float  
+text = "Code"   # string
+is_ready = True # boolean
+
+print(x, type(x))
+print(y, type(y))
+print(text, type(text))
+print(is_ready, type(is_ready))
