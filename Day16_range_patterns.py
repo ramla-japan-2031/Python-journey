e06cf498bf344969df7611f5a251ead91b6d1c2b
Ramla beevi <muhsinaramlaz@gmail.com>
2026-06-16 18:24:11 +0530

    Create Day16_range_patterns.py
    Day 16: range() + nested loop + triangle patterns

---
+++ Day16_range_patterns.py
@@ -0,0 +1,11 @@
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+print("---")  
+ 
+for i in range(5, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
