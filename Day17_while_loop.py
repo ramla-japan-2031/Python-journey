2952c10dc555f40e0f87f22f39950a51919d8d89
Ramla beevi <muhsinaramlaz@gmail.com>
2026-06-17 19:36:13 +0530

    Create Day17_while_loop.py
    Day 17: While loops - counting, sum 1-10, password validation

---
+++ Day17_while_loop.py
@@ -0,0 +1,24 @@
+
+count = 1
+
+while count <= 5:  
+    print("Count:", count)
+    count = count + 1  
+
+print("Loop finished!")
+
+
+total = 0
+num = 1
+while num <=10:
+ total = total + num
+ num = num +1 
+print("Sum:",total)
+ 
+ 
+password = ""
+while password != "japan2031":
+    password = input("Enter password: ")
+    
+print("Access granted! Welcome Ramla 🇯🇵")
+ 
